d415cd3474d4abaddc2fa1defe60f543f0436b9f
atharvpatwardhan <atharvjpatwardhan@gmail.com>
2025-03-28 18:14:14 -0400

    Add fine-tuned model and inferencing code.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO("yolov8n.pt")
+model = YOLO("datasets/runs/detect/train2/weights/best.pt")
 
 cap = cv2.VideoCapture(0)
 
@@ -13,9 +13,26 @@ while cap.isOpened():
 
     results = model(frame, show=True)
 
-    annotated_frame = results[0].plot()
+    for r in results:
+        for box in r.boxes:
+            cls = int(box.cls[0])
+            conf = float(box.conf[0])
+            x1,y1,x2,y2 = map(int,box.xyxy[0])
 
-    cv2.imshow("YOLOv8 Detection", annotated_frame)
+            label = f"{model.names[cls]} {conf:.2f}"
+
+            color = (0,255,0) if cls == 0 else (255,0,0)
+
+            cv2.rectangle(frame, (x1,y1), (x2,y2), color, 2)
+            cv2.putText(frame, label, (x1,y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+            if cls == 1:
+                print("Drowsiness detected!")
+                cv2.putText(frame, "Drowsiness detected!", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+
+
+    cv2.imshow("Drowsiness Detection", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
