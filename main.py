cabd0e236e54fc2fcbca452f9660c30fae8095dc
atharvpatwardhan <atharvjpatwardhan@gmail.com>
2025-03-29 19:13:54 -0400

    Add whatsapp alert feature.

--- main.py
+++ main.py
@@ -3,13 +3,30 @@ from pyspark.sql.functions import col
 from pyspark.streaming import StreamingContext
 from pyspark.sql.types import StringType
 from kafka import KafkaProducer
-import torch
 import cv2
 import base64
 import numpy as np
 from ultralytics import YOLO
 import boto3
 import json
+from dotenv import load_dotenv
+import os
+import requests
+
+
+load_dotenv()
+
+
+def send_whatsapp_alert():
+    CALLMEBOT_API_KEY = os.getenv("CALLMEBOT_API_KEY")
+    PHONE_NUMBER = os.getenv("ALERT_PHONE_NUMBER")
+    MESSAGE = "🚨+Drowsiness+Alert+!+Take+a+break!"
+
+    url = f"https://api.callmebot.com/whatsapp.php?phone={PHONE_NUMBER}&text={MESSAGE}&apikey={CALLMEBOT_API_KEY}"
+    response = requests.get(url)
+
+    print(f"Message sent! Status: {response.status_code}")
+
 
 spark = SparkSession.builder \
     .appName("DrowsinessDetection") \
@@ -50,6 +67,7 @@ def process_frame(batch_df, batch_id):
                     label = r.names[int(box.cls)]
                     if label in ["drowsy"]:  # Adjust based on dataset labels
                         alert = True
+                        send_whatsapp_alert()
                         producer.send("drowsiness_alerts", {"alert": "Drowsiness Detected"})
                         file_name = f"drowsyframes/{batch_id}.jpg"
                         s3_client.put_object(Bucket=S3_BUCKET, Key=file_name, Body=buffer.tobytes())
@@ -71,42 +89,3 @@ query = df.writeStream \
 
 query.awaitTermination()
 
-
-
-
-# cap = cv2.VideoCapture(0)
-
-# while cap.isOpened():
-#     ret,frame = cap.read()
-
-#     if not ret:
-#         break
-
-#     results = model(frame, show=True)
-
-#     for r in results:
-#         for box in r.boxes:
-#             cls = int(box.cls[0])
-#             conf = float(box.conf[0])
-#             x1,y1,x2,y2 = map(int,box.xyxy[0])
-
-#             label = f"{model.names[cls]} {conf:.2f}"
-
-#             color = (0,255,0) if cls == 0 else (255,0,0)
-
-#             cv2.rectangle(frame, (x1,y1), (x2,y2), color, 2)
-#             cv2.putText(frame, label, (x1,y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-#             if cls == 1:
-#                 print("Drowsiness detected!")
-#                 cv2.putText(frame, "Drowsiness detected!", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-
-
-#     cv2.imshow("Drowsiness Detection", frame)
-
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-
-# cap.release()
-# cv2.destroyAllWindows()
